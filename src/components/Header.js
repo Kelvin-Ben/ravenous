4bc4393127f58a2a38eb1eb2ce2e8a63c449affc
Kelvin-Ben <kevoben14@gmail.com>
2024-06-19 20:21:44 +0300

    Create a header component with the app logo text title

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="w-full h-[5rem] flex items-center justify-center bg-yellow-600 absolute top-0">
+      <h1 className="text-white text-4xl capitalize">ravenous</h1>
+    </div>
+  );
+};
+
+export default Header;
