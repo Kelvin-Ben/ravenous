dfcb29923b0f922f67c4ea9a689e5b97c86b6c7e
Kelvin-Ben <kevoben14@gmail.com>
2024-06-19 20:24:00 +0300

    Create the home page to render all the components of the application

---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../components/Hero";
+import BusinessList from "../components/BusinessList";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <BusinessList />
+    </div>
+  );
+};
+
+export default HomePage;
