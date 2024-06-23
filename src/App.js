b6f6eccebca251f42c0a65b2a265d719add6cf2a
Kelvin-Ben <kevoben14@gmail.com>
2024-06-23 19:40:08 +0300

    Follow React pattern by importing json data in top level component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import HomePage from "./pages/HomePage";
+import data from '../src/data.json'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <HomePage data={data} />
     </div>
   );
 }
