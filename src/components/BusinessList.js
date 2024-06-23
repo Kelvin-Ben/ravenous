854aa0a553aa64f31071c1faefd1a49133473d89
Kelvin-Ben <kevoben14@gmail.com>
2024-06-23 19:42:37 +0300

    Pass data as prop from top level component

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,10 +1,8 @@
 import React from "react";
-import data from "../data.json";
 import Business from "./Business";
 
-const BusinessList = () => {
+const BusinessList = ({data}) => {
   const { businesses } = data;
-  console.log(businesses);
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
